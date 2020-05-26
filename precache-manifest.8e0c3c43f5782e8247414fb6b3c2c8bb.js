690d83e5f513bce94595b58f40c9c2bfbcd3ca9f
Kirin Sarangkasiri <k4kinny@gmail.com>
2020-05-26 16:19:56 +0700

    Updates

---
+++ precache-manifest.8e0c3c43f5782e8247414fb6b3c2c8bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8cc8d9226608f441df4cd350d96617",
+    "url": "/coronatable/index.html"
+  },
+  {
+    "revision": "1985bf6a943495c00122",
+    "url": "/coronatable/static/css/main.66d8a312.chunk.css"
+  },
+  {
+    "revision": "e7aa356ce90605dc2701",
+    "url": "/coronatable/static/js/2.c76405bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coronatable/static/js/2.c76405bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1985bf6a943495c00122",
+    "url": "/coronatable/static/js/main.a335a4bc.chunk.js"
+  },
+  {
+    "revision": "eb35a160506ca76ebe6c",
+    "url": "/coronatable/static/js/runtime-main.2a84d7c2.js"
+  }
+]);
